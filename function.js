69f739bb2c357a539070f15f4d0b112453488347
robertojff <robertojff3333@gmail.com>
2023-12-16 20:51:40 -0300

    first commit

---
+++ function.js
@@ -0,0 +1,40 @@
+let corSite = 'azul';
+function resetarCor(cor){
+    corSite = cor;
+}
+
+console.log(corSite);
+
+let corSegundoSite ='amarelo'
+function resetaCor(cor){
+    corSegundoSite = cor;
+}
+
+console.log(corSegundoSite);
+
+let corTerceiroSite ='Vermelho'
+function resetaCor(cor){
+    corTerceiroSite = cor;
+}
+
+console.log(corTerceiroSite);
+
+let corQuartoSite ='Preto'
+function resetaCor(cor, tonalidade){
+    corQuartoSite = cor+ ' ' +tonalidade;
+}
+
+console.log(corQuartoSite);
+resetaCor("branco","claro")
+console.log(corQuartoSite);
+
+let corQuintoSite ='Roxo'
+function resetarCor(cor){
+    corQuintoSite = cor;
+}
+
+console.log(corQuintoSite);
+resetarCor("Verde");
+console.log(corQuintoSite);
+
+
